d86ecb6ea6a3c9d205b818b13439c4e813a60531
sedlarski <sedlarski@yahoo.com>
2024-01-06 17:33:03 -0500

    adding the models

---
+++ user.core/src/main/java/com/springbank/user/core/models/User.java
@@ -0,0 +1,22 @@
+package com.springbank.user.core.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+    private String firstname;
+    private String lastname;
+    private String emailAddress;
+    private Account account;
+}
