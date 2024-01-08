a634cd5ba62546c91bee4d1d9b4c25b0c1b1e429
sedlarski <sedlarski@yahoo.com>
2024-01-08 17:45:19 -0500

    adding user queries

---
+++ user.query.api/src/main/java/com/springbank/user/query/api/queries/FindUserByIdQuery.java
@@ -0,0 +1,8 @@
+package com.springbank.user.query.api.queries;
+
+import lombok.Data;
+
+@Data
+public class FindUserByIdQuery {
+    private String id;
+}
---
+++ user.query.api/src/main/java/com/springbank/user/query/api/queries/SearchUsersQuery.java
@@ -0,0 +1,8 @@
+package com.springbank.user.query.api.queries;
+
+import lombok.Data;
+
+@Data
+public class SearchUsersQuery {
+    private String filter;
+}
