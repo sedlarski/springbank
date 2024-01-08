571176d6b7b6c0d79d596a7d2560de828d45c8fc
sedlarski <sedlarski@yahoo.com>
2024-01-08 18:05:19 -0500

    adding user query Handlers

---
+++ user.query.api/src/main/java/com/springbank/user/query/api/handlers/UserQueryHandllerImpl.java
@@ -0,0 +1,45 @@
+package com.springbank.user.query.api.handlers;
+
+import com.springbank.user.query.api.dto.UserLookupResponse;
+import com.springbank.user.query.api.queries.FindAllUsersQuery;
+import com.springbank.user.query.api.queries.FindUserByIdQuery;
+import com.springbank.user.query.api.queries.SearchUsersQuery;
+import com.springbank.user.query.api.repositories.UserRepository;
+import org.axonframework.queryhandling.QueryHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserQueryHandllerImpl implements UserQueryHandler{
+
+    private final UserRepository userRepository;
+
+
+    @Autowired
+    public UserQueryHandllerImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @QueryHandler
+    @Override
+    public UserLookupResponse getUserById(FindUserByIdQuery query) {
+        var users = userRepository.findById(query.getId());
+        return users.isPresent() ? new UserLookupResponse(users.get()) : null;
+    }
+
+    @QueryHandler
+    @Override
+    public UserLookupResponse searchUsers(SearchUsersQuery query) {
+        var users = new ArrayList<>(userRepository.findByFilterRegex(query.getFilter()));
+        return new UserLookupResponse(users);
+    }
+
+    @QueryHandler
+    @Override
+    public UserLookupResponse getAllUsers(FindAllUsersQuery query) {
+        var users = new ArrayList<>(userRepository.findAll());
+        return new UserLookupResponse(users);
+    }
+}
