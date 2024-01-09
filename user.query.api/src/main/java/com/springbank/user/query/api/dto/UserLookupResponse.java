6c48b2e2f3bcf4b17fff959aad0ebb4751f24da7
sedlarski <sedlarski@yahoo.com>
2024-01-09 17:36:45 -0500

    adding user query Controllers

--- user.query.api/src/main/java/com/springbank/user/query/api/dto/UserLookupResponse.java
+++ user.query.api/src/main/java/com/springbank/user/query/api/dto/UserLookupResponse.java
@@ -1,5 +1,6 @@
 package com.springbank.user.query.api.dto;
 
+import com.springbank.user.core.dto.BaseResponse;
 import com.springbank.user.core.models.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,14 +8,37 @@ import lombok.Data;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
-@AllArgsConstructor
-public class UserLookupResponse {
+
+public class UserLookupResponse extends BaseResponse {
     private List<User> users;
 
-    public UserLookupResponse(User user) {
+    public UserLookupResponse(String message) {
+        super(message);
+    }
+
+    public UserLookupResponse(List<User> users) {
+        super(null);
+        this.users = users;
+    }
+
+    public UserLookupResponse(String message, User user) {
+        super(message);
         this.users = new ArrayList<>();
         this.users.add(user);
 
+    }
+    public UserLookupResponse(User user) {
+        super(null);
+        this.users = new ArrayList<>();
+        this.users.add(user);
+
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
     }
 }
